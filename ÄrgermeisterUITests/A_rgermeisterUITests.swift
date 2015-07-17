b3e8fc8c34b201d2459d7d1808dc4d3d2114bda4
huw <huw1999@gmail.com>
2015-07-17 10:09:23 +1000

    Initial Commit

---
+++ ÄrgermeisterUITests/A_rgermeisterUITests.swift
@@ -0,0 +1,34 @@
+//
+//  A_rgermeisterUITests.swift
+//  ÄrgermeisterUITests
+//
+//  Created by Huw on 2015-07-17.
+//  Copyright © 2015 Huw. All rights reserved.
+//
+
+import XCTest
+
+class A_rgermeisterUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
