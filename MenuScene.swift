0c02006daa02fcb9a139060286c17fe1ad108841
huw <huw1999@gmail.com>
2015-08-11 21:07:37 +1000

    Saving fully implemented, new bigger map

--- MenuScene.swift
+++ MenuScene.swift
@@ -20,7 +20,35 @@ class MenuScene: SKScene {
     
     let spin = SKAction.repeatActionForever(SKAction.rotateByAngle(CGFloat(-2 * M_PI), duration: 4))
     
+    func save() {
+        
+        // Saving the characters is done through very few, very simple
+        // lines of code. These ones save the 'characters' variable in
+        // the `NSUserDefaults` archive. Were you to scroll below, you
+        // would notice that we do the reverse when loading. Elsewhere
+        // (in Player()), we save the things we want to preserve using
+        // NSCoder, which encodes them into a saveable format that can
+        // be safely stored without pesky users altering the values to
+        // cheat. When saving using NSKeyedArchiver, the program looks
+        // at the options in init(NSCoder), and uses that.
+        
+        let savedData = NSKeyedArchiver.archivedDataWithRootObject(characters)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(savedData, forKey: "characters")
+    }
+    
     override func didMoveToView(view: SKView) {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        // Code to wipe the NSUserDefaults in case something messes up
+        /*let ident = NSBundle.mainBundle().bundleIdentifier!
+        defaults.removePersistentDomainForName(ident)*/
+        
+        // Load the characters from the NSUserDefaults storage
+        if let savedCharacters = defaults.objectForKey("characters") as? NSData {
+            characters = NSKeyedUnarchiver.unarchiveObjectWithData(savedCharacters) as! [Player]
+        }
         
         setLabels()
         
@@ -28,29 +56,73 @@ class MenuScene: SKScene {
     }
     
     override func keyDown(e: NSEvent) {
-        let key: NSString = e.charactersIgnoringModifiers!
+        let code = e.keyCode
         
-        if key == "a" {
+        // As we're trying to detect a carriage return, we need to 
+        // change the way we detect keypresses. Normally, we'd try
+        // to find the char using `e.charactersIgnoringModifiers!`
+        // but that doesn't detect a return. Instead we can change
+        // the key into an integer, which gives us the right value
+        // ('a' == 0, 'd' == 2, enter == 36)
+        
+        if code == 0 {
             // Check the currently selected character to make sure
             // we don't move over the edge of the screen
             
-            if selectedCharacter - 1 >= 0 {
+            if !(characters.count <= selectedCharacter - 2) && selectedCharacter - 1 >= 0 {
                 selectedCharacter--
-            } else {
-                selectedCharacter = 2
+                changeSpin(selectedCharacter)
             }
             
-            changeSpin(selectedCharacter)
+        } else if code == 2 {
             
-        } else if key == "d" {
-            
-            if selectedCharacter + 1 <= 2 {
+            if !(characters.count <= selectedCharacter) && selectedCharacter + 1 <= 2 {
                 selectedCharacter++
-            } else {
-                selectedCharacter = 0
+                changeSpin(selectedCharacter)
             }
             
-            changeSpin(selectedCharacter)
+        } else if code == 36 {
+            if characters.count - 1 >= selectedCharacter {
+                save()
+                
+                // If this is the case, then we are over a playable character
+                let scene = LevelScene(size: CGSize(width: 1024, height: 768))
+                scene.scaleMode = .AspectFill
+                scene.player = characters[selectedCharacter]
+                scene.save = self
+                
+                // Build a camera, and add it to the scene
+                let newCamera = SKCameraNode()
+                scene.camera = newCamera
+                scene.addChild(newCamera)
+                
+                let transition = SKTransition.crossFadeWithDuration(1)
+                
+                self.scene!.view?.presentScene(scene, transition: transition)
+            } else {
+                let newCharacter = Player()
+                
+                let colors = [SKColor.yellowColor(), SKColor.brownColor(), SKColor.cyanColor(), SKColor.magentaColor(), SKColor.purpleColor(), SKColor.whiteColor()]
+                let names = ["Sir Yellow", "Dr Brown", "Mrs Cyan", "Prof. Magenta", "Lady Purple", "Walter White"]
+                
+                let index = Int(random(colors.count))
+                
+                newCharacter.color = colors[index]
+                newCharacter.name = names[index]
+                
+                characters.append(newCharacter)
+                setLabels()
+                
+                save()
+            }
+        } else if code == 51 {
+            // Delete the current character
+            if characters.count - 1 >= selectedCharacter {
+                characters.removeAtIndex(selectedCharacter)
+                setLabels()
+                
+                save()
+            }
         }
     }
     
@@ -80,30 +152,72 @@ class MenuScene: SKScene {
             let current = characters[0]
             character1.color = current.color
             name1.text = current.name
-            level1.text = "\(floor(current.level))"
-            health1.text = "\(current.maxHealth)"
-            speed1.text = "\(current.attackSpeed)"
-            defence1.text = "\(current.defence)"
+            level1.text = "Level \(Int(floor(current.level)))"
+            health1.text = "Health: \(current.maxHealth)"
+            speed1.text = "Speed: \(Int(floor(current.attackSpeed)))"
+            defence1.text = "Defence: \(current.defence)"
             
             if characters.count >= 2 {
                 let current = characters[1]
                 character2.color = current.color
                 name2.text = current.name
-                level2.text = "\(floor(current.level))"
-                health2.text = "\(current.maxHealth)"
-                speed2.text = "\(current.attackSpeed)"
-                defence2.text = "\(current.defence)"
+                level2.text = "Level \(Int(floor(current.level)))"
+                health2.text = "Health: \(current.maxHealth)"
+                speed2.text = "Speed: \(Int(floor(current.attackSpeed)))"
+                defence2.text = "Defence: \(current.defence)"
                 
                 if characters.count >= 3 {
                     let current = characters[2]
                     character3.color = current.color
                     name3.text = current.name
-                    level3.text = "\(floor(current.level))"
-                    health3.text = "\(current.maxHealth)"
-                    speed3.text = "\(current.attackSpeed)"
-                    defence3.text = "\(current.defence)"
+                    level3.text = "Level \(Int(floor(current.level)))"
+                    health3.text = "Health: \(current.maxHealth)"
+                    speed3.text = "Speed: \(Int(floor(current.attackSpeed)))"
+                    defence3.text = "Defence: \(current.defence)"
+                } else {
+                    character3.color = SKColor.redColor()
+                    name3.text = "Add New"
+                    level3.text = "Level ???"
+                    health3.text = "Health: ???"
+                    speed3.text = "Speed: ???"
+                    defence3.text = "Defence: ???"
                 }
+            } else {
+                character2.color = SKColor.redColor()
+                name2.text = "Add New"
+                level2.text = "Level ???"
+                health2.text = "Health: ???"
+                speed2.text = "Speed: ???"
+                defence2.text = "Defence: ???"
+                
+                character3.color = SKColor.redColor()
+                name3.text = "Add New"
+                level3.text = "Level ???"
+                health3.text = "Health: ???"
+                speed3.text = "Speed: ???"
+                defence3.text = "Defence: ???"
             }
+        } else {
+            character1.color = SKColor.redColor()
+            name1.text = "Add New"
+            level1.text = "Level ???"
+            health1.text = "Health: ???"
+            speed1.text = "Speed: ???"
+            defence1.text = "Defence: ???"
+            
+            character2.color = SKColor.redColor()
+            name2.text = "Add New"
+            level2.text = "Level ???"
+            health2.text = "Health: ???"
+            speed2.text = "Speed: ???"
+            defence2.text = "Defence: ???"
+            
+            character3.color = SKColor.redColor()
+            name3.text = "Add New"
+            level3.text = "Level ???"
+            health3.text = "Health: ???"
+            speed3.text = "Speed: ???"
+            defence3.text = "Defence: ???"
         }
     }
     
